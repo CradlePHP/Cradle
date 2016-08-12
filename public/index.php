61f838d4d5c90caf98d88996473d52f6c3767401
Christian Blanquera <cblanquera@openovate.com>
2016-08-12 13:11:00 +0800

    fixed issue with php5.6
     - Apparently 5.6 doesn’t like weird.

--- public/index.php
+++ public/index.php
@@ -1,19 +1,20 @@
 <?php //-->
 
-//embrace the wierd
-return (include(__DIR__.'/../bootstrap.php'))
+include(__DIR__.'/../bootstrap.php');
+
+return cradle()
 	//add routes here
 	->get('/', 'Hello World')
-	
+
 	//add flows here
 	->flow(
-		'Hello World', 
+		'Hello World',
 		function($request, $response) {
 			$message1 = '<h1>Welcome to Cradle!</h1>';
 			$message2 = '<p>Now remove this process flow :)</p>';
 			$response->setContent($message1 . $message2);
 		}
 	)
-	
+
 	//start rendering
 	->render();
--- worker.php
+++ worker.php
@@ -1,8 +1,9 @@
 <?php //-->
 use PhpAmqpLib\Exception\AMQPProtocolChannelException;
 
-//embrace the wierd
-return (include(__DIR__.'/bootstrap.php'))
+include(__DIR__.'/../bootstrap.php');
+
+return cradle()
 	//add a logger
 	->addLogger(function($message) {
 		echo '[cradle] ' . $message . PHP_EOL;
@@ -10,30 +11,30 @@ return (include(__DIR__.'/bootstrap.php'))
 	//register the task runner
 	->preprocess(function($request, $response) {
 		static $channel = null;
-		
-		//get the channel	
+
+		//get the channel
 		if(is_null($channel)) {
 			$channel = $this
 				->package('global')
 				->service('queue-main')
 				->channel();
 		}
-		
+
 		//get the queue name
 		$settings = $this->package('global')->config('settings');
 		$name = 'queue';
 		if(isset($settings['queue']) && trim($settings['queue'])) {
 			$name = $settings['queue'];
 		}
-		
+
 		// notify its up
         $this->log('Waiting for tasks.');
-		
+
 		// define the job
         $job = function($message) use ($name, $request, $response) {
             // notify once a task is received
             $this->log('A task is received.');
-            
+
 			// get the data
             $data = json_decode($message->body, true);
 
@@ -47,37 +48,37 @@ return (include(__DIR__.'/bootstrap.php'))
                 $message
 					->delivery_info['channel']
 					->basic_nack($message->delivery_info['delivery_tag']);
-					
+
 				// set or flag that the task is not done and the worker is free and requeue task
                 //$message->delivery_info['channel']->basic_nack($message->delivery_info['delivery_tag'], false, true);
 			}
-            
+
 			$task = $data['__TASK__'];
 			unset($data['__TASK__']);
-			
+
             try {
                 //start
 				$this->log($task . ' is running');
-				
+
 				$request->setPost($data);
-			
+
 				$this->triggerEvent($task, $request, $response);
-				
+
 				//if there was an error
 				if($response->get('body', 'error')) {
 					$error = $response->getDot('body.message');
-					
+
 					$this->log('Task is not done.');
 					$this->log($error);
 					$this->log(json_encode($data));
-					
+
 					//an exception didn't trigger
 					//it just refused to do it
 					//so why try it again ?
-				} else {			
+				} else {
 					$this->log($task . ' was performed');
 					$this->log(json_encode($data));
-	
+
 					// once done, notify again, that it is done
 					$this->log('Task is done.');
 				}
@@ -97,33 +98,33 @@ return (include(__DIR__.'/bootstrap.php'))
 					->basic_nack($message->delivery_info['delivery_tag']);
             }
         };
-		
+
 		// worker consuming tasks from queue
         $channel->basic_qos(null, 1, null);
-        
+
         // now we need to catch the channel exception
         // when task does not exists in our queue
         try {
             // comsume messages on queue
             $channel->basic_consume(
-				$name, 
-				'', 
-				false, 
-				false, 
-				false, 
-				false, 
+				$name,
+				'',
+				false,
+				false,
+				false,
+				false,
 				$job->bindTo($this)
 			);
         } catch(AMQPProtocolChannelException $e) {
             // notify that task does not exists
-            $this->log('Task does not exists, creating task. Please re-run the worker.');    
-            
+            $this->log('Task does not exists, creating task. Please re-run the worker.');
+
 			// create the init queue
 			$this
 				->package('global')
 				->queue('init');
         }
-        
+
         while(count($channel->callbacks)) {
             $channel->wait();
         }
