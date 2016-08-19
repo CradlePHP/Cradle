ecc8662ccf018254273ee32eb6725a1b0aeedd09
Christian Blanquera <cblanquera@openovate.com>
2016-08-19 17:10:12 +0800

    this throws errors

--- config/services.php
+++ config/services.php
@@ -1,5 +1,5 @@
 <?php //-->
 
 return array (
-    'sql-main' => new PDO('mysql:host=127.0.0.1;dbname=framework', 'root', '')
+    //'sql-main' => new PDO('mysql:host=127.0.0.1;dbname=framework', 'root', '')
 );
