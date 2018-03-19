241f338e543cef54f0fc974fefd7d09cee9d762d
Christian Blanquera <cblanquera@openovate.com>
2018-03-19 12:46:48 +0800

    event fix

--- app/www/.cradle.php
+++ app/www/.cradle.php
@@ -3,7 +3,7 @@
  * This file is part of a Custom Project.
  */
 include_once __DIR__ . '/src/controller/static.php';
-include_once __DIR__ . '/src/package/events.php';
+include_once __DIR__ . '/src/events.php';
 
 //bootstrap
 $this->preprocess(include __DIR__ . '/src/bootstrap.php');
