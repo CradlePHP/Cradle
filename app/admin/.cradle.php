e88ae172a8c17e865e487dfca6902bd1ee0ac31c
Charles Zamora <charles.andacc@gmail.com>
2018-03-23 18:58:24 +0800

    bring back the admin templates page

--- app/admin/.cradle.php
+++ app/admin/.cradle.php
@@ -5,6 +5,7 @@
 require_once __DIR__ . '/src/events.php';
 require_once __DIR__ . '/src/controller/configuration.php';
 require_once __DIR__ . '/src/controller/menu.php';
+require_once __DIR__ . '/src/controller/static.php';
 
 //bootstrap
 $this->preprocess(include __DIR__ . '/src/bootstrap.php');
