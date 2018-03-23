53aea131006c956c57457f6ca555aadedc4b9d47
Charles Zamora <charles.andacc@gmail.com>
2018-03-23 18:51:50 +0800

    bring back the configuration page

--- app/admin/.cradle.php
+++ app/admin/.cradle.php
@@ -3,6 +3,7 @@
  * This file is part of a Custom Project.
  */
 require_once __DIR__ . '/src/events.php';
+require_once __DIR__ . '/src/controller/configuration.php';
 require_once __DIR__ . '/src/controller/menu.php';
 
 //bootstrap
