8ecfbb2cbb54f9bb1ba96501e2c3deb0592c1981
Charles Zamora <charles.andacc@gmail.com>
2018-03-27 18:35:46 +0800

    branch out package installer

--- app/install/.cradle.php
+++
@@ -1,9 +0,0 @@
-<?php //-->
-/**
- * This file is part of a Custom Project.
- */
-require_once __DIR__ . '/src/events.php';
-require_once __DIR__ . '/src/controller/package.php';
-
-//bootstrap
-$this->preprocess(include __DIR__ . '/src/bootstrap.php');
--- app/install/src/events.php
+++
@@ -1,8 +0,0 @@
-<?php //-->
-/**
- * This file is part of a Custom Project.
- * (c) 2017-2019 Acme Inc.
- *
- * Copyright and license information can be found at LICENSE.txt
- * distributed with this package.
- */
