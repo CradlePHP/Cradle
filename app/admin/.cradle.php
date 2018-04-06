3d0fa687d6578aeaca24b3e55708c1ceeb3f0c28
vneugenio <voltairegit@gmail.com>
2018-04-06 15:48:33 +0800

    renamed files to .cradle.php from bootstrap.php #18 ~2.5h

--- app/admin/.cradle.php
+++ app/admin/.cradle.php
@@ -8,4 +8,4 @@ require_once __DIR__ . '/src/controller/menu.php';
 require_once __DIR__ . '/src/controller/static.php';
 
 //bootstrap
-$this->preprocess(include __DIR__ . '/src/bootstrap.php');
+$this->preprocess(include __DIR__ . '/src/.cradle.php');
