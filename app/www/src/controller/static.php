16e00ec6add372aba4282401c2fa5dbb3fe13d7e
Charles Zamora <charles.andacc@gmail.com>
2018-04-13 14:04:24 +0800

    file downloader / preview link

--- app/www/src/controller/static.php
+++ app/www/src/controller/static.php
@@ -1,5 +1,7 @@
 <?php //-->
 
+use Cradle\Module\Utility\File;
+
 /**
  * Render the Home Page
  *
@@ -23,4 +25,39 @@ $this->get('/', function ($request, $response) {
 
     //Render blank page
     $this->trigger('www-render-page', $request, $response);
+});
+
+/**
+ * Render the file preview / download
+ * 
+ * @param Request $request
+ * @param Response $response
+ */
+$this->get('/download', function ($request, $response) {
+    // get file location
+    $location = $request->getStage('location');
+    // get file name
+    $filename = $request->getStage('filename');
+
+    // get file mime type
+    $mime = File::getMimeFromLink($location);
+
+    // set file type
+    $type = sprintf('%s; charset=UTF-8', $mime);
+
+    $response
+        ->addHeader('Content-Encoding', 'UTF-8')
+        ->addHeader('Content-Type', $type);
+
+    // force download?
+    if ($request->hasStage('force')) {
+        // let the browser download the file
+        $response->addHeader('Content-Disposition', 'attachment; filename=' . $filename);
+    } else {
+        // this is just a preview
+        $response->addHeader('Content-Disposition', 'inline; filename=' . $filename);
+    }
+
+    // load the file
+    $response->setContent(file_get_contents($location));
 });
